3abc891a3c242d0386badfde33c606dabeaa8df7
Chris Apolzon <apolzon@gmail.com>
2012-01-06 22:39:00 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pareview::Application.config.secret_token = 'a0fefad1057b4996c4596798bb47e416a760830e62fa8065b252fcecd08c8a3ce9b1e579213bc905c017e30e51162b33050c74fcb145c29033d38a59f12b05cc'
